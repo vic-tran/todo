6d71d3d3b149b3fb0bc8530959bcd53d4da28403
Victor Tran <vc.tran998@gmail.com>
2022-11-20 21:24:02 +0700

    Add styling to buttons and todo functions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import { AiOutlinePlus } from "react-icons/ai";
 import Todo from "./Todo";
 
   const style = {
-    bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`
+    bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#EB6440] to-[#497174]`,
+    container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+    heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+    form: `flex justify-between`,
+    input: `border p-2 w-full text-xl`,
+    button: `border p-4 ml-2 bg-green-300 text-slate-100`,
+    count: `text-center p-2`,
   }
 
 function App() {
@@ -24,6 +30,7 @@ function App() {
             <Todo key={index} todo={todo}/>
           ))}
         </ul>
+        <p className={style.count}>You have 2 todo(s)</p>
       </div>
     </div>
   );
